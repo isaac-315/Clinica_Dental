98abb10457dc7a063f82fe61a6feb043d643bf52
isaac-315 <isaacalf.135@gmail.com>
2025-12-26 15:04:26 -0500

    Primer commit v0.0.1

---
+++ src/main/java/paquete1/clinica_dental/Clinica_Dental.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package paquete1.clinica_dental;
+
+/**
+ *
+ * @author USUARO_PC
+ */
+public class Clinica_Dental {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
